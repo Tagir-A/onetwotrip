5b05d9dccef18ffbb7f819503561ef240e30be92
Tagir.A <tccup1@gmail.com>
2017-05-10 08:23:16 +0300

    change time format

--- src/components/dumbs/flightCard.js
+++ src/components/dumbs/flightCard.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
 function FlightCard({ flight }) {
+    let departure = new Date(flight.departure).toLocaleString("ru-RU")
+    let arrival = new Date(flight.arrival).toLocaleString("ru-RU")
     return (
         <li>
             <p className="from">{flight.direction.from}</p>
             <p className="to">{flight.direction.to}</p>
-            <p className="departure">{flight.departure}</p>
-            <p className="arrival">{flight.arrival}</p>
+            <p className="departure">{departure}</p>
+            <p className="arrival">{arrival}</p>
             <p className="carrier">{flight.carrier}</p>
         </li>
     )
